8bf52a71cb370e76ffa6020a90e6be87332e843e
𝙿𝚁𝙸𝙼𝙴 𝙰𝙻𝙿𝙷𝙰-𝙼𝙳 𝚅 01.0 <primealpha727@gmail.com>
2025-08-04 00:08:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Q4AjUbLQ#18HDoz61VAXpNqAq7vsrZM8gYLAXyK2aYxA6TuBrICg",
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94706042889",
+SESSION_ID: process.env.SESSION_ID || "&qItSCQyI#8QLMiRwQDVCw-wYPhFiXqHUm47djPrmfgi7zEUL0WcI",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94770349867",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
-ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
-ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> ",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/1tQYm6zR/4z-S9-P9-Rhm9.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM PRIME ALPHA-XMD CREATED BY SHERON <NOW ALIVE> ",
 };
